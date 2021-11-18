98c894e46a5d134f60ee63305597cb583941aaa8
lendi156 <lendi156@gmail.com>
2021-11-18 21:13:32 +0700

    add next and previous button

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from 'react'
-import { Container, Stack } from '@mui/material'
+import { Container, Stack, Button } from '@mui/material'
 import StoriesList from '../Component/StoriesList'
 
 export default function Home () {
   const [topStoryList, seTopStoryList] = useState([])
-  const [first] = useState(0)
-  const [last] = useState(10)
+  const [first, setFirst] = useState(0)
+  const [last, setLast] = useState(10)
   const getTopStoryData = async () => {
     const response = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
     const responseJson = await response.json()
     seTopStoryList([...responseJson])
+  }
+
+  const nextPage = (first, last) => {
+    setFirst(first + 10)
+    setLast(last + 10)
+  }
+
+  const prevPage = (first, last) => {
+    setFirst(first - 10)
+    setLast(last - 10)
   }
 
   useEffect(() => {
@@ -31,6 +41,10 @@ export default function Home () {
     <Stack spacing={2} sx={{ margin: '80px 0 20px' }}>
       {storyList}
     </Stack>
+    <Stack direction='row' justifyContent='flex-end' spacing={2}>
+            {first === 0 ? null : <Button variant="contained" onClick={() => prevPage(first, last, setFirst, setLast)}>Previous</Button>}
+            {last === topStoryList.length ? null : <Button variant="contained" onClick={() => nextPage(first, last, setFirst, setLast)}>Next</Button>}
+          </Stack>
     </Container>
   )
 }
